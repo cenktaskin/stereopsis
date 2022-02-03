b916dfb3fa60ccaffa829d5e0a6a970a14abf9a9
Cenk T <orhuncenktaskin@gmail.com>
2022-02-03 16:09:11 +0100

    tweaks before transfer to gitlab

--- scripts/scratch.py
+++ scripts/scratch.py
@@ -1,7 +1,12 @@
-import pandas as pd
 import pickle
-import cv2 as cv
+import numpy as np
+from pathlib import Path
+from PIL import Image
 
+data_path = Path('/home/cenkt/Downloads/undistorted.pickle')
+data = pickle.load(open(data_path, 'rb'))
 
-
-
+for i, example in enumerate(data):
+    normalized_label = np.round((example[1]-example[1].min())/(example[1].max()-example[1].min()) * 255)
+    Image.fromarray(normalized_label).convert('L').save(f"dataset/dist/{i:05}.png")
+    Image.fromarray(example[0]).save(f"dataset/image/{i:05}.png")
