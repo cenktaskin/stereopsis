6660f8c0bbc83b522f72b4663dfa14f0a883a996
Cenk T <orhun.taskin@tuhh.de>
2022-06-02 17:31:21 +0200

    progress bar fix

--- src/train_the_model.py
+++ src/train_the_model.py
@@ -1,12 +1,31 @@
 from datetime import datetime
 import pytz
 import numpy as np
+from tqdm import tqdm
 
 import torch
+from torch.utils.data import DataLoader
+from torchvision import transforms
+
 from preprocessing.data_io import data_path
+from dataset import LabelTransformer, StereopsisDataset, np_to_tensor
 from loss import MaskedMSE
 from src.beeline.model import BeelineModel
-from train_wrappers import create_dataloaders, train_the_model
+from torch.utils.tensorboard import SummaryWriter
+
+
+def test(dataloader, model, loss_fn, device="cuda"):
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss = 0
+    with torch.no_grad():
+        for x1, x2, y in dataloader:
+            x1, x2, y = x1.to(device), x2.to(device), y.to(device)
+            pred = model(x1, x2)
+            test_loss += loss_fn(pred, y).item()
+    test_loss /= num_batches
+    # print(f"Test Error: \n Avg loss: {test_loss:>8f} \n")
+    return test_loss
 
 
 dataset_id = "20220301"
@@ -20,17 +39,63 @@ if not results_path.exists():
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using {device} device")
 
-train_dataloader, test_dataloader, test_idx = create_dataloaders(dataset_path, batch_size=32, test_split_ratio=0.9)
+# Create dataloaders
+batch_size = 8
+test_split_ratio = 0.9
+label_transformer = LabelTransformer(h=120, w=214)
+dataset = StereopsisDataset(dataset_path, transform=transforms.Compose([np_to_tensor]),
+                            target_transform=transforms.Compose([label_transformer]))
+
+train_size = int(test_split_ratio * len(dataset))
+test_size = len(dataset) - train_size
+train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
+
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+test_idx = test_dataset.indices
+#
 
 model = BeelineModel().to(device)
-
 loss_fn = MaskedMSE()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.05)
 
-epochs = 200
-model, train_hist, test_hist = train_the_model(model, epochs, train_dataloader, test_dataloader, loss_fn, optimizer, device)
+# Train the model
+epochs = 2
+#train_hist, test_hist = [], []
+batch_count = len(train_dataloader)
+print(f"Batch count: {batch_count}")
+for i in range(epochs):
+    train_loop = tqdm(train_dataloader, leave=False)
+    train_loop.set_description(f"Epoch [{i:4d}/{epochs:4d}]")
 
-torch.save(model.state_dict(), results_path.joinpath("model.pth"))
-np.savetxt(results_path.joinpath('test_indices.txt'), test_idx)
-np.savetxt(results_path.joinpath('train_hist.txt'), train_hist)
-np.savetxt(results_path.joinpath('test_hist.txt'), test_hist)
+    model.train()
+    running_loss = 0
+    for batch, (x1, x2, y) in enumerate(train_loop):
+        x1, x2, y = x1.to(device), x2.to(device), y.to(device)
+
+        prediction = model(x1, x2)
+        loss = loss_fn(prediction, y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+        reporting_interval = 1
+        if batch % reporting_interval == reporting_interval-1:
+            last_loss = running_loss / reporting_interval
+            #print(f"Batch [{batch+1:4d}/{batch_count:4d}] Loss: {last_loss:.4f}")
+            #sample_idx = i * len(train_dataloader) + i + 1
+            running_loss = 0
+            train_loop.set_postfix(loss=f"{last_loss:.4f}")
+
+    #test_loss = test(test_dataloader, model, loss_fn, device)
+    #train_loop.set_postfix(loss=train_loss, test_loss=test_loss)
+    #train_hist.append(train_loss)
+    #test_hist.append(test_loss)
+print("Done!")
+
+#torch.save(model.state_dict(), results_path.joinpath("model.pth"))
+#np.savetxt(results_path.joinpath('test_indices.txt'), test_idx)
+#np.savetxt(results_path.joinpath('train_hist.txt'), train_hist)
+#np.savetxt(results_path.joinpath('test_hist.txt'), test_hist)
