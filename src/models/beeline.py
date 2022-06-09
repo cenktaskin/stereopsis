fec0c2e58129c59b1b5faba8524ed7b1fa00474c
Cenk T <orhun.taskin@tuhh.de>
2022-06-09 21:42:43 +0200

    deeper baseline model

--- src/models/beeline.py
+++ src/models/beeline.py
@@ -1,28 +1,27 @@
 from torch import nn
 
 
-class NeuralNetworkModel(nn.Module):
+class NNModel(nn.Module):
     name = "beeline"
 
     def __init__(self):
-        super(NeuralNetworkModel, self).__init__()
-        self.conv1 = nn.Sequential(nn.Conv2d(6, 64, 3, stride=3, padding=1),
-                                   nn.BatchNorm2d(3),
+        super(NNModel, self).__init__()
+        self.conv1 = nn.Sequential(nn.Conv2d(6, 32, kernel_size=7, stride=3, padding=3),
+                                   nn.BatchNorm2d(32),
                                    nn.ReLU())
-        self.conv2 = nn.Sequential(nn.Conv2d(3, 3, 3, stride=2, padding=1),
-                                   nn.BatchNorm2d(3),
+        self.conv2 = nn.Sequential(nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1),
+                                   nn.BatchNorm2d(64),
                                    nn.ReLU())
-        self.conv3 = nn.Conv2d(3, 1, 3, padding='same')
+        self.conv3 = nn.Sequential(nn.Conv2d(64, 128, kernel_size=3, padding='same'),
+                                   nn.BatchNorm2d(128),
+                                   nn.ReLU())
+        self.conv4 = nn.Conv2d(128, 1, kernel_size=3, padding='same')
 
-    def forward(self, l, r):
-        l = l.float()
-        r = r.float()
-        # l_img, r_img = split(x, x.shape[2] // 2, dim=2)
+    def forward(self, x):
+        x = x.float()
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
 
-        x1 = self.conv1(l)
-
-        x2 = self.conv1(r)
-
-        stacked = x1 + x2
-        y = self.conv2(stacked)
-        return y
+        return x
