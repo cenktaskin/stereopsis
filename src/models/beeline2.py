64291fa4437e626b69dd4f30df0697da973cf4bc
Cenk Taskin <orhun.taskin@tuhh.de>
2022-06-09 22:46:33 +0200

    fix on batchnorm argument forgotten

--- src/models/beeline2.py
+++ src/models/beeline2.py
@@ -7,13 +7,13 @@ class NNModel(nn.Module):
     def __init__(self):
         super(NNModel, self).__init__()
         self.conv1 = nn.Sequential(nn.Conv2d(6, 64, kernel_size=7, stride=3, padding=3),
-                                   nn.BatchNorm2d(32),
-                                   nn.ReLU())
-        self.conv2 = nn.Sequential(nn.Conv2d(32, 128, kernel_size=3, stride=2, padding=1),
                                    nn.BatchNorm2d(64),
                                    nn.ReLU())
-        self.conv3 = nn.Sequential(nn.Conv2d(64, 256, kernel_size=3, padding='same'),
+        self.conv2 = nn.Sequential(nn.Conv2d(32, 128, kernel_size=3, stride=2, padding=1),
                                    nn.BatchNorm2d(128),
+                                   nn.ReLU())
+        self.conv3 = nn.Sequential(nn.Conv2d(64, 256, kernel_size=3, padding='same'),
+                                   nn.BatchNorm2d(256),
                                    nn.ReLU())
         self.conv4 = nn.Conv2d(128, 1, kernel_size=3, padding='same')
 
