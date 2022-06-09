cfc4df61ef5e0fa2a8e82b51caf57c2518ff7d87
Cenk Taskin <orhun.taskin@tuhh.de>
2022-06-09 22:43:54 +0200

    more filters on hidden layer on beeline

---
+++ src/models/beeline2.py
@@ -0,0 +1,27 @@
+from torch import nn
+
+
+class NNModel(nn.Module):
+    name = "beeline2"
+
+    def __init__(self):
+        super(NNModel, self).__init__()
+        self.conv1 = nn.Sequential(nn.Conv2d(6, 64, kernel_size=7, stride=3, padding=3),
+                                   nn.BatchNorm2d(32),
+                                   nn.ReLU())
+        self.conv2 = nn.Sequential(nn.Conv2d(32, 128, kernel_size=3, stride=2, padding=1),
+                                   nn.BatchNorm2d(64),
+                                   nn.ReLU())
+        self.conv3 = nn.Sequential(nn.Conv2d(64, 256, kernel_size=3, padding='same'),
+                                   nn.BatchNorm2d(128),
+                                   nn.ReLU())
+        self.conv4 = nn.Conv2d(128, 1, kernel_size=3, padding='same')
+
+    def forward(self, x):
+        x = x.float()
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+
+        return x
