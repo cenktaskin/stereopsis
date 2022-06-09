4cfbe8e37edcc4c4b85ae629a4b736ab3105993e
Cenk T <orhun.taskin@tuhh.de>
2022-06-09 21:43:43 +0200

    calculation for deeper beeline, deleted trial file

--- src/shape_calculator.py
+++ src/shape_calculator.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def calculate_output_shape(inp_shape, c_out, k, s, p, d=1):
+def calculate_output_shape(inp_shape, c_out, k, s:int, p, d=1):
     c_in, h_in, w_in = inp_shape
     print(f"Input shape {inp_shape}")
 
@@ -22,22 +22,9 @@ def calculate_output_shape(inp_shape, c_out, k, s, p, d=1):
     return out_shape
 
 
-# think about un-equal stride to make image closer to square
-input_dims = (3, 720, 1280)  # C x H x W
-out_dims_1 = calculate_output_shape(input_dims, c_out=3, k=3, s=2, p=1)
-out_dims_2 = calculate_output_shape(out_dims_1, c_out=1, k=3, s=3, p=1)
-#out_dims_3 = calculate_output_shape(out_dims_2, c_out=3, k=3, s=(2, 1), p=1)
-
-print("********")
 dims = (6, 720, 1280)  # C x H x W
-dims = calculate_output_shape(dims, c_out=64, k=7, s=3, p=3)
-dims = calculate_output_shape(dims, c_out=1, k=3, s=2, p=1)
-dims = calculate_output_shape(dims, c_out=1, k=3, s=1, p=1)
+dims = calculate_output_shape(dims, c_out=32, k=7, s=3, p=3)
+dims = calculate_output_shape(dims, c_out=64, k=3, s=2, p=1)
+dims = calculate_output_shape(dims, c_out=128, k=3, s=1, p=1)
 dims = calculate_output_shape(dims, c_out=1, k=3, s=1, p=1)
 
-print("********")
-dims = (6, 720, 1280)  # C x H x W
-dims = calculate_output_shape(dims, c_out=64, k=7, s=(2,3), p=3)
-dims = calculate_output_shape(dims, c_out=1, k=3, s=2, p=1)
-dims = calculate_output_shape(dims, c_out=1, k=3, s=1, p=1)
-dims = calculate_output_shape(dims, c_out=1, k=3, s=1, p=1)
