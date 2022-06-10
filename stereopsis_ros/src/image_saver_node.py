181c4229fb51c8646ec16e6d4eb93734594b4dff
Cenk Taskin <orhun.taskin@tuhh.de>
2022-06-10 14:29:58 +0200

    changes on data capture, gitignore fix

--- stereopsis_ros/src/image_saver_node.py
+++ stereopsis_ros/src/image_saver_node.py
@@ -1,70 +1,65 @@
 #! /usr/bin/python3
 
 import rospy
-from sensor_msgs.msg import Image
+from sensor_msgs import msg
 from cv_bridge import CvBridge, CvBridgeError
 import message_filters
 
 import cv2
+import pytz
+from datetime import datetime
 from pathlib import Path
 
 
 class ImageSaverNode:
-    output_file_path = Path('/home/cenkt/ros/stereopsis-ws/outputs/')
-
-    def __init__(self, st_topic, ir_topic, dp_topic, ir_topic8, simult=True):
-        self.bridge = CvBridge()
-        self.image_count = 0
-
-        if simult:
-            st_sub = message_filters.Subscriber(st_topic, Image)
-            ir_sub = message_filters.Subscriber(ir_topic, Image)
-            dp_sub = message_filters.Subscriber(dp_topic, Image)
-            ir_sub8 = message_filters.Subscriber(ir_topic8, Image)
-            ts = message_filters.ApproximateTimeSynchronizer([st_sub, ir_sub, dp_sub, ir_sub8], 10, 0.01)
-            ts.registerCallback(self.callback_poly)
-        else:
-            st_sub = rospy.Subscriber(st_topic, Image, self.callback_mono)
-
-    def callback_mono(self, msg):
-        try:
-            cv2_img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-        except CvBridgeError as e:
-            print(e)
-        else:
-            time = msg.header.stamp
-            cv2.imwrite(str(ImageSaverNode.output_file_path.joinpath('st_' + str(time) + '.tiff')), cv2_img)
-            self.image_count += 1
-            print(f"Saved {self.image_count} image(s)")
-            rospy.sleep(1)
-
-    def callback_poly(self, stereo_msg, ir_msg, dp_msg, ir2_msg):
-        try:
-            stereo_img = self.bridge.imgmsg_to_cv2(stereo_msg, "bgr8")
-            ir_img = self.bridge.imgmsg_to_cv2(ir_msg, "16UC1")
-            ir_img8 = self.bridge.imgmsg_to_cv2(ir2_msg, "8UC1")
-            depth_img = self.bridge.imgmsg_to_cv2(dp_msg, "32FC1")
-        except CvBridgeError as e:
-            print(e)
-        else:
-            time_stamp = stereo_msg.header.stamp
-            cv2.imwrite(str(ImageSaverNode.output_file_path.joinpath('st_' + str(time_stamp) + '.tiff')), stereo_img)
-            cv2.imwrite(str(ImageSaverNode.output_file_path.joinpath('ir_' + str(time_stamp) + '.tiff')), ir_img)
-            cv2.imwrite(str(ImageSaverNode.output_file_path.joinpath('i8_' + str(time_stamp) + '.tiff')), ir_img8)
-            cv2.imwrite(str(ImageSaverNode.output_file_path.joinpath('dp_' + str(time_stamp) + '.tiff')), depth_img)
-            self.image_count += 1
-            print(f"Saved {self.image_count} image(s)")
-            rospy.sleep(1)
-
-
-if __name__ == '__main__':
     stereo_topic = '/stereo_driver_node/image_raw'
     ir_topic = '/pico_flexx/image_mono16'
     ir_topic8 = '/pico_flexx/image_mono8'
     depth_topic = '/pico_flexx/image_depth'
+    ir_format = '16UC1'
+    depth_format = '32FC1'
+
+    def __init__(self, calibration=False, output_dir='/home/cenkt/ros-outputs/'):
+        timestamp = datetime.now().astimezone(pytz.timezone("Europe/Berlin")).strftime("%Y%m%d%H%M")
+        self.output_path = Path(output_dir).joinpath(f"dataset-{timestamp}")
+        if not self.output_path.exists():
+            self.output_path.mkdir()
+        self.bridge = CvBridge()
+        self.image_count = 0
+        self.calibration = calibration
+        self.st_sub = message_filters.Subscriber(ImageSaverNode.stereo_topic, msg.Image)
+
+        pico_topic = ImageSaverNode.depth_topic
+        self.pico_format = ImageSaverNode.depth_format
+        self.pico_prefix = 'dp_'
+        if calibration:
+            pico_topic = ImageSaverNode.ir_topic
+            self.pico_format = ImageSaverNode.ir_format
+            self.pico_prefix = 'ir_'
+
+        self.pico_sub = message_filters.Subscriber(pico_topic, msg.Image)
+        ts = message_filters.ApproximateTimeSynchronizer([self.st_sub, self.pico_sub], 10, 0.1)
+        ts.registerCallback(self.callback_poly)
+
+    def callback_poly(self, stereo_img, pico_img):
+        try:
+            stereo_img = self.bridge.imgmsg_to_cv2(stereo_img, "bgr8")
+            pico_img = self.bridge.imgmsg_to_cv2(pico_img, self.pico_format)
+        except CvBridgeError as e:
+            print(e)
+        else:
+            time_stamp = stereo_img.header.stamp
+            cv2.imwrite(str(self.output_path.joinpath('st_' + str(time_stamp) + '.tiff')), stereo_img)
+            cv2.imwrite(str(self.output_path.joinpath(self.pico_prefix + str(time_stamp) + '.tiff')), pico_img)
+            self.image_count += 1
+            print(f"Saved {self.image_count} image(s)")
+            rospy.sleep(0.01)
+
+
+if __name__ == '__main__':
     rospy.init_node('image_saver')
     print("Node initialized")
-    ImageSaverNode(stereo_topic, ir_topic, depth_topic, ir_topic8)
+    ImageSaverNode(calibration=rospy.get_param('calibration', False))
     while not rospy.is_shutdown():
         rospy.spin()
     print("Terminated")
