b002215466ae6830cb0425678fbdff497c296b51
Cenk Taskin <orhun.taskin@tuhh.de>
2022-06-12 15:03:10 +0200

    generalized beeline for ease of reproduction

--- src/models/beeline.py
+++ src/models/beeline.py
@@ -1,27 +1,30 @@
 from torch import nn
+from collections import OrderedDict
 
 
 class NNModel(nn.Module):
-    name = "beeline"
-
-    def __init__(self):
+    def __init__(self, name, c_list):
         super(NNModel, self).__init__()
-        self.conv1 = nn.Sequential(nn.Conv2d(6, 32, kernel_size=7, stride=3, padding=3),
-                                   nn.BatchNorm2d(32),
-                                   nn.ReLU())
-        self.conv2 = nn.Sequential(nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1),
-                                   nn.BatchNorm2d(64),
-                                   nn.ReLU())
-        self.conv3 = nn.Sequential(nn.Conv2d(64, 128, kernel_size=3, padding='same'),
-                                   nn.BatchNorm2d(128),
-                                   nn.ReLU())
-        self.conv4 = nn.Conv2d(128, 1, kernel_size=3, padding='same')
+        self.name = name
+
+        layers = OrderedDict()
+        layers[f"conv_{0}"] = conv_layer(c_in=6, c_out=c_list[0], k=7, s=3, p=3)
+        layers[f"conv_{1}"] = conv_layer(c_in=c_list[0], c_out=c_list[1], s=2, p=1)
+        for i in range(len(c_list) - 2):
+            layers[f"conv_{i + 2}"] = conv_layer(c_in=c_list[i + 1], c_out=c_list[i + 2])
+        layers["conv_last"] = nn.Conv2d(c_list[-1], 1, kernel_size=3, padding="same")
+
+        self.layers = nn.Sequential(layers)
 
     def forward(self, x):
         x = x.float()
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-
+        for layer in self.layers:
+            x = layer(x)
         return x
+
+
+def conv_layer(c_in, c_out, k=3, s=1, p="same"):
+    print(f"created {c_in}->{c_out} with {k, s, p}")
+    return nn.Sequential(nn.Conv2d(c_in, c_out, kernel_size=k, stride=s, padding=p),
+                         nn.BatchNorm2d(c_out),
+                         nn.ReLU())
