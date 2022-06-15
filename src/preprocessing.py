e5eee87601256d8a03b2c54c868ee9e934eb1961
Cenk Taskin <orhun.taskin@tuhh.de>
2022-06-15 17:51:08 +0200

    re-wrote the preprocessing module for cropping and resizing to aspect ratio of 2

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from dataset import data_path
 from tqdm import tqdm
@@ -5,31 +6,56 @@ from cv2 import cv2
 import os
 
 
-class LabelTransformer(object):
-    def __init__(self, h, w):
-        self.desired_h = h
-        self.desired_w = w
+class ImageResizer(object):
+    """first crops the height to fit the aspect ratio then resizes
+    ->assumes raw image height needs to be cropped, can be done robuster"""
+
+    def __init__(self, target_size, verbose=False):
+        self.initial_h = None
+        self.initial_w = None
+        self.target_shape = target_size
+        self.target_aspect_ratio = self.target_shape[1] / self.target_shape[0]
         self.h_slice = None
-        self.w_slice = None
+        self.verbose = verbose
 
-    def crop_label(self, sample):
-        if self.h_slice is None:
-            reduce_h_amount = (sample.shape[0] - self.desired_h)
-            self.h_slice = slice(reduce_h_amount // 2, self.desired_h + reduce_h_amount // 2)
-            self.w_slice = slice(sample.shape[1] - self.desired_w, sample.shape[1])
-        return sample[self.h_slice, self.w_slice]
+    def init_raw_size(self, sample):
+        self.initial_h = sample.shape[0]
+        self.initial_w = sample.shape[1]
+        self.calculate_slices()
 
-    def __call__(self, sample):
-        return self.crop_label(sample)
+    def calculate_slices(self):
+        h_crop_target = int(self.initial_w / self.target_aspect_ratio)
+        reduce_h_amount = self.initial_h - h_crop_target
+        self.h_slice = slice(reduce_h_amount // 2, h_crop_target + reduce_h_amount // 2)
+
+    def crop_img(self, img):
+        return img[self.h_slice, :]
+
+    def resize_img(self, img):
+        return cv2.resize(img, self.target_shape[::-1], interpolation=cv2.INTER_AREA)
+
+    def __call__(self, img):
+        if not self.h_slice:
+            self.init_raw_size(img)
+        cropped = self.crop_img(img)
+        if self.verbose:
+            print(f"Cropped {img.shape} -> {cropped.shape}")
+        if cropped.shape == self.target_shape:
+            return cropped
+        resized = self.resize_img(cropped)
+        if self.verbose:
+            print(f"Resized {img.shape} -> {resized.shape}")
+        return resized
 
 
 # after making sure it works, you can turns this into glob
 acquired_datasets = ["202206101932", "202206101937", "202206101612"]
-data_dirs = [data_path.joinpath(f"raw/dataset-{d}") for d in acquired_datasets]
+data_dirs = [data_path.joinpath(f"raw/rawdata-{d}") for d in acquired_datasets]
 
 total_imgs = sum([sum([len(files) for _, _, files in os.walk(i)]) for i in data_dirs]) // 2
 
-label_transformer = LabelTransformer(120, 214)
+sample_resizer = ImageResizer((384, 768))
+label_resizer = ImageResizer((112, 224))
 
 clean_dataset_path = data_path.joinpath(f"processed/dataset-20220610/")
 if not clean_dataset_path.exists():
@@ -39,16 +65,19 @@ with tqdm(total=total_imgs) as pbar:
     for data_dir in data_dirs:
         for img_path in data_dir.glob("st_*.tiff"):
             ts = img_path.stem[3:]
-            image = cv2.imread(img_path.as_posix())
-            if not image.any():
+            raw_image = cv2.imread(img_path.as_posix())
+            if not raw_image.any():
                 continue
+
+            raw_left, raw_right = np.split(raw_image, 2, axis=1)
             label_path = img_path.with_stem(f"dp_{ts}")
-            label = cv2.imread(label_path.as_posix(), flags=cv2.IMREAD_UNCHANGED)
+            raw_label = cv2.imread(label_path.as_posix(), flags=cv2.IMREAD_UNCHANGED)
 
-            l_img, r_img = np.split(image, 2, axis=1)
-            label = label_transformer(label)
+            img_left = sample_resizer(raw_left)
+            img_right = sample_resizer(raw_right)
+            img_label = label_resizer(raw_label)
 
-            cv2.imwrite(clean_dataset_path.joinpath(f"sl_{ts}.tiff").as_posix(), l_img)
-            cv2.imwrite(clean_dataset_path.joinpath(f"sr_{ts}.tiff").as_posix(), r_img)
-            cv2.imwrite(clean_dataset_path.joinpath(f"dp_{ts}.tiff").as_posix(), label)
+            cv2.imwrite(clean_dataset_path.joinpath(f"sl_{ts}.tiff").as_posix(), img_left)
+            cv2.imwrite(clean_dataset_path.joinpath(f"sr_{ts}.tiff").as_posix(), img_right)
+            cv2.imwrite(clean_dataset_path.joinpath(f"dp_{ts}.tiff").as_posix(), img_label)
             pbar.update(1)
