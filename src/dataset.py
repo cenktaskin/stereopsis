fa1745d7eb2f42138a7d76da4102701b194d6524
Cenk Taskin <orhun.taskin@tuhh.de>
2022-06-20 20:09:36 +0200

    converted cv2 code to pil for performance increase

--- src/dataset.py
+++ src/dataset.py
@@ -1,8 +1,9 @@
 from pathlib import Path
+import math
 
 import numpy as np
 import matplotlib.pyplot as plt
-from cv2 import cv2
+from PIL import Image
 
 import torch
 from torch.utils.data import DataLoader, Dataset
@@ -15,26 +16,35 @@ data_path = project_path.joinpath('data')
 class StereopsisDataset(Dataset):
     def __init__(self, img_dir):
         self.img_dir = img_dir
-        self.transform = transforms.Compose([transforms.ToTensor(),
-                                             transforms.Normalize([0.485, 0.456, 0.406],
-                                                                  [0.229, 0.224, 0.225])])
-        self.target_transform = None
+        self.left_stats, self.right_stats, self.label_stats = self.get_input_stats()
+        print(self.left_stats)
+        self.left_transform = transforms.Compose([transforms.ToTensor(),
+                                                  transforms.Normalize(self.left_stats[:3], self.left_stats[3:])])
+        self.right_transform = transforms.Compose([transforms.ToTensor(),
+                                                   transforms.Normalize(self.right_stats[:3], self.right_stats[3:])])
+        self.target_transform = transforms.Compose([transforms.ToTensor(),
+                                                    transforms.Normalize(self.label_stats[0], self.label_stats[1])])
         self.timestamp_list = np.array(sorted([int(x.stem[3:]) for x in self.img_dir.glob("sl*")]))
 
     def __len__(self):
         return len(list(self.img_dir.glob("sl*")))
+
+    def get_input_stats(self):
+        stats = np.loadtxt(self.img_dir.joinpath("stats.txt"))
+        return stats[:6], stats[6:-2], stats[-2:]
 
     def ts_to_index(self, ts):
         return np.where(np.equal(self.timestamp_list, ts))[0][0]
 
     def __getitem__(self, idx):
         ts = self.timestamp_list[idx]
-        image_l = cv2.cvtColor(cv2.imread(self.img_dir.joinpath(f"sl_{ts}.tiff").as_posix()), cv2.COLOR_BGR2RGB)
-        image_r = cv2.cvtColor(cv2.imread(self.img_dir.joinpath(f"sr_{ts}.tiff").as_posix()), cv2.COLOR_BGR2RGB)
-        label = cv2.imread(self.img_dir.joinpath(f"dp_{ts}.tiff").as_posix(), flags=cv2.IMREAD_UNCHANGED)
-        if self.transform:
-            image_l = self.transform(image_l)
-            image_r = self.transform(image_r)
+        image_l = Image.open(self.img_dir.joinpath(f"sl_{ts}.tiff").as_posix())
+        image_r = Image.open(self.img_dir.joinpath(f"sr_{ts}.tiff").as_posix())
+        label = Image.open(self.img_dir.joinpath(f"dp_{ts}.tiff").as_posix())
+        if self.left_transform:
+            image_l = self.left_transform(image_l)
+        if self.right_transform:
+            image_r = self.right_transform(image_r)
         if self.target_transform:
             label = self.target_transform(label)
         return torch.cat([image_l, image_r]), label
@@ -53,25 +63,23 @@ def imshow(inp, title=None):
     plt.pause(0.01)  # pause a bit so that plots are updated
 
 
-def plot_res(list_of_pics):
-    i = 1
-    for pic in list_of_pics:
-        plt.subplot(3, 3, i)
-        if i < 3:
-            inp = pic.numpy().transpose((1, 2, 0))
-            mean = np.array([0.485, 0.456, 0.406])
-            std = np.array([0.229, 0.224, 0.225])
-            inp = std * inp + mean
-            inp = np.clip(inp, 0, 1)
-        else:
-            inp = pic
-        plt.imshow(inp)
-        i += 1
+def show_images(imgs, titles, tensor=True, row_count=1, col_count=None):
+    if not col_count:
+        col_count = math.ceil(len(imgs) / row_count)
+    for i, img in enumerate(imgs):
+        plt.subplot(row_count, col_count, i + 1)
+        try:
+            plt.title(f"{titles[i]} - max:{img.max():.4f}")
+        except:
+            pass
+        if tensor:
+            img = img.permute(1, 2, 0)
+        plt.imshow(img, interpolation=None)
     plt.show()
 
 
 if __name__ == "__main__":
-    data_path = data_path.joinpath("raw/dataset-20220610/")
+    data_path = data_path.joinpath("processed/dataset-20220610-fullres/")
 
     dataset = StereopsisDataset(data_path)
     dataloader = DataLoader(dataset, batch_size=16, shuffle=True)
