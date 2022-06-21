87855d2ccaa135efaae5ade737def5bfe2dc3aff
Cenk Taskin <orhun.taskin@tuhh.de>
2022-06-21 15:52:32 +0200

    some more shell scripts

---
+++ bin/create-venv.sh
@@ -0,0 +1,4 @@
+python3.10 -m venv ~/.venvs/env-stereopsis/
+. ~/.venvs/env-stereopsis/bin/activate
+CC="cc -mavx2" pip install -U --force-reinstall pillow-simd
+pip install -r ~/git/stereopsis/env-recipe.txt
