d3843eb99bcbc3947f742302ed774744232266e6
Cenk Taskin <orhun.taskin@tuhh.de>
2022-06-21 17:04:01 +0200

    change on recipe for pillow-simd, and an addition to venv creator

--- bin/create-venv.sh
+++ bin/create-venv.sh
@@ -1,4 +1,5 @@
 python3.10 -m venv ~/.venvs/env-stereopsis/
 . ~/.venvs/env-stereopsis/bin/activate
+pip install --upgrade pip
 CC="cc -mavx2" pip install -U --force-reinstall pillow-simd
 pip install -r ~/git/stereopsis/env-recipe.txt
