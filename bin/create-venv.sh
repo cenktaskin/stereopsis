1886c74425e06f72e97ca295ee63596f14c596b5
Cenk Taskin <orhun.taskin@tuhh.de>
2022-06-22 18:40:06 +0200

    cleared pillow-simd dependency

--- bin/create-venv.sh
+++ bin/create-venv.sh
@@ -1,5 +1,4 @@
 python3.10 -m venv ~/.venvs/env-stereopsis/
 . ~/.venvs/env-stereopsis/bin/activate
 pip install --upgrade pip
-CC="cc -mavx2" pip install -U --force-reinstall pillow-simd
 pip install -r ~/git/stereopsis/env-recipe.txt
--- bin/install-ubuntu-requirements.sh
+++ bin/install-ubuntu-requirements.sh
@@ -1,8 +1,3 @@
 # python3.10
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt install python3.10 python3.10-venv
-# pillow-simd reqs
-sudo apt-get install python3.10-dev
-sudo apt-get install libtiff5-dev libjpeg8-dev libopenjp2-7-dev zlib1g-dev \
-    libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python3-tk \
-    libharfbuzz-dev libfribidi-dev libxcb1-dev
