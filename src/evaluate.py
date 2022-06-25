090966e9a1de47474bac78b5bafe08d7d8beb82d
Cenk Taskin <orhun.taskin@tuhh.de>
2022-06-26 00:00:22 +0200

    a checkp before refactoring with new dataset saving

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,34 +1,71 @@
 import argparse
+import torch
+import numpy as np
 
 from net_tester import tester
-from dataset import data_path
+from dataset import data_path, StereopsisDataset
 
 arg_parser = argparse.ArgumentParser(description="NN Evaluator")
 arg_parser.add_argument("-r", "--run-name", type=str)
-arg_parser.add_argument("-dt", "--dataset-type", type=str, default="fullres")
+arg_parser.add_argument("-chdt", "--choose-dataset", action='store_true')
+arg_parser.add_argument("-chw", "--choose-weights", action='store_true')
 arg_parser.add_argument("-o", "--old-type-idx", action='store_true')
 args = arg_parser.parse_args()
 
-log_path = None
-matching_runs = list(data_path.joinpath("logs").glob(f"*{args.run_name}*"))
-if len(matching_runs) == 1:
-    log_path = matching_runs[0]
-else:
-    if len(matching_runs) == 0:
-        print("No matching run found, choose one:")
-        possible_logs = list(data_path.joinpath("logs").glob("*"))
-    else:
-        print("More than one matching run found, choose one:")
-        possible_logs = matching_runs
-    for i, p in enumerate(possible_logs):
+
+def offer_choices(options):
+    for i, p in enumerate(options):
         print(f"{i}) {p.name}")
     try:
         idx = int(input("Choice: "))
-        log_path = possible_logs[idx]
+        return options[idx]
     except ValueError:
         exit("Unexpected input type!")
     except IndexError:
         exit("Didn't match!")
 
-del args.run_name
-tester(log_path, **vars(args))
+
+possible_runs = list(data_path.joinpath("logs").glob(f"*{args.run_name}*"))
+if len(possible_runs) == 1:
+    log_path = possible_runs[0]
+else:
+    if len(possible_runs) == 0:
+        print("No matching run found, checking parent dir:")
+        possible_runs = list(data_path.joinpath("logs").glob("*"))
+    else:
+        print("More than one matching run found, choose one:")
+    log_path = offer_choices(possible_runs)
+
+possible_sets = list(log_path.parent.parent.glob(f"processed/dataset-*"))
+if not args.choose_dataset:
+    dataset_path = possible_sets[-1]
+else:
+    print(f"Choose a dataset:")
+    dataset_path = offer_choices(possible_sets)
+
+possible_weights = list(log_path.glob("model*.pt"))
+if len(possible_weights) == 0:
+    weight_path = None
+    exit("No matching weights found, exiting...")
+elif len(possible_weights) == 1:
+    weight_path = possible_weights[0]
+else:
+    if not args.choose_weights:
+        weight_path = possible_weights[-1]
+    else:
+        print("More than one weight file is found, choose one:")
+        weight_path = offer_choices(possible_weights)
+
+val_loader_idx = torch.load(log_path.joinpath("val_loader.pt"))
+print(val_loader_idx)
+# val_loader_idx.dataset = StereopsisDataset(dataset_path)
+# for j, (x, y) in enumerate(val_loader_idx):
+#    print(j)
+# exit()
+# .dataset.indices
+if args.old_type_idx:
+    val_loader_idx = np.loadtxt(log_path.joinpath("validation_indices.txt")).astype(int)
+
+dataset = StereopsisDataset(dataset_path)
+
+tester(weight_path, dataset, val_loader_idx)
--- src/net_tester.py
+++ src/net_tester.py
@@ -2,36 +2,30 @@ import numpy as np
 import torch
 
 from models import dispnet
-from dataset import StereopsisDataset, show_images
+from dataset import show_images
 from loss import MaskedEPE, MultilayerSmoothL1viaPool
 
 
-def tester(log_path, dataset_type, old_type_idx):
+def tester(weights_path, dataset, val_idx):
     current_device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    val_loader_idx = torch.load(log_path.joinpath("val_loader.pt")).dataset.indices
-    if old_type_idx:
-        val_loader_idx = np.loadtxt(log_path.joinpath("validation_indices.txt")).astype(int)
-
-    dataset_path = log_path.parent.parent.joinpath(f"processed/dataset-20220610-{dataset_type}")
-    dataset = StereopsisDataset(dataset_path)
-
-    model_weights = torch.load(next(log_path.glob("model*")), map_location=current_device)
     model = dispnet.NNModel(batch_norm=True)
+    model_weights = torch.load(weights_path, map_location=current_device)
     model.load_state_dict(model_weights)
 
     acc_fn = MaskedEPE()
     loss_fn = MultilayerSmoothL1viaPool()
     model.eval()
     with torch.no_grad():
-        for idx in val_loader_idx:
-            x, y = dataset.__getitem__(idx)
-            x_tensor = torch.from_numpy(x).unsqueeze(dim=0).float()
-            y_tensor = torch.from_numpy(y).unsqueeze(dim=0).float()
+        for idx in val_idx:
+            sample, label = dataset.__getitem__(idx)
+            x_tensor = torch.from_numpy(sample).unsqueeze(dim=0).float()
+            y_tensor = torch.from_numpy(label).unsqueeze(dim=0).float()
             y_hats = model(x_tensor)
-            pred = y_hats[-1].squeeze()
             loss = loss_fn(y_hats, y_tensor, stage=3)
             accuracy = acc_fn(y_hats, y_tensor)
             tit = ["left_img", "right_img", "label", "pred"]
-            main_tit = f"Loss:{loss:.4f}, Accuracy:{accuracy:.4f}"
-            show_images([*np.split(x.squeeze(), 2, axis=0), y, pred], titles=tit, row_count=2, main_title=main_tit)
+            prediction = y_hats[-1].numpy().squeeze()
+            print(sample.shape)
+            show_images([*np.split(sample.squeeze(), 2, axis=0), label, prediction], titles=tit, row_count=2,
+                        main_title=f"{weights_path.name} \n Loss:{loss:.4f}, EPE:{accuracy:.4f}")
