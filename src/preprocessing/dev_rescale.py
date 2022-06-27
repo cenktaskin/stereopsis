3862ed15b7349f7cbe51b4605f981acb4c884f26
Cenk Taskin <orhun.taskin@tuhh.de>
2022-06-27 14:32:50 +0200

    removed temporary rescale script

--- src/preprocessing/dev_rescale.py
+++
@@ -1,9 +0,0 @@
-from data_io import data_path, MultipleDirDataHandler
-import cv2
-
-data_id = "processed/dataset-20220610-registered"
-data_dir = data_path.joinpath(data_id)
-a = MultipleDirDataHandler(data_id, ("sl", "sr", "dp"))
-
-for ts, raw_st, raw_depth in a.iterate_over_imgs():
-    cv2.imwrite(data_dir.joinpath(f"dp_{ts}.tiff").as_posix(), raw_depth / 1000)
