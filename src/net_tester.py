7f19cd044edcea0dd18ce55095ad4db83643671a
Cenk Taskin <orhun.taskin@tuhh.de>
2022-06-28 08:09:39 +0200

    fix on import statement for net_tester.py

--- src/net_tester.py
+++ src/net_tester.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 from torch.nn.functional import interpolate
 
-from dataset import show_images
+from preprocessing.data_io import show_images
 from loss import MaskedEPE, MultilayerSmoothL1viaPool, MultilayerSmoothL1
 
 
